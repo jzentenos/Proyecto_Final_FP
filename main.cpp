fd8f91bc1942a940553662eda268412ff787260b
acatarip <acatarip@unjbg.edu.pe>
2024-08-04 18:53:33 -0500

    main.cpp : Se agrego la base para el sub-menu PACIENTE y se añadio una pausa al ingresar una opcion invalida

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+void Paciente ();
+
 int main (){
 	int n;
 	do {
@@ -16,7 +18,7 @@ int main (){
 		
 		switch (n){
 			case 1:
-				cout<<"\nAhora esta en Sub-menu PACIENTE.";
+				Paciente ();
 				break;
 			case 2:
 				cout<<"\nAhora esta en Sub-menu DOCTORES.";
@@ -28,11 +30,41 @@ int main (){
 				cout<<"\nSaliendo...";
 				break;
 			default:
-				cout<<"\nOpcion invalida. Ingrese su opcion de nuevo: ";
+				cout<<"\nOpcion invalida, intentelo de nuevo."<<endl;
+				system("pause");
 				break;
 		}
 		
 	} while (n!=0);
 	
 	return 0;
+}
+	
+void Paciente (){
+	int a;
+	do {
+		cout<<"\n--------------------"<<endl;
+		cout<<"Sub-menu PACIENTE"<<endl;
+		cout<<"--------------------"<<endl;
+		cout<<"1. Agregar Paciente"<<endl;
+		cout<<"2. Lista de pacientes registrados"<<endl;
+		cout<<"0. Regresar al Menu Principal -> "<<endl;
+		cout<<"Ingrese opcion: "; cin>> a;
+		
+		switch (a){
+			case 1:
+				cout<<"Ahora esta en Agregar paciente";
+				break;
+			case 2:
+				cout<<"Ahora esta en Lista de pacientes registrados";
+				break;
+			case 0:
+				cout<<"Regresando al Menu Principal...";
+				break;
+			default:
+				cout<<"Opcion invalida, intentelo de nuevo"<<endl;
+				system("pause");
+				break;
+		}
+	} while (a!=0);
 }
