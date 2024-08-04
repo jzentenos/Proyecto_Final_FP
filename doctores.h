10e26362422e9138bcae1bc87d0b0c48d7d0ba2f
jzentenos <jzentenos@unjbg.edu.pe>
2024-08-03 21:54:30 -0500

    Se creo la cabezera doctores.h(Incluye el registro de cada doctor)

---
+++ doctores.h
@@ -0,0 +1,12 @@
+#ifndef DOCTORES_H
+#define DOCTORES_H
+#include<string>
+struct INFORMACION{
+	string nombres;
+	string apellidos;
+	char telefono[100];
+	string especialidad;
+	string estado_civil;
+	string pais;
+};
+#endif
