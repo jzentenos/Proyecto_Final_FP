31e985238beb9c40b94f42aeca9952397552c05b
jzentenos <jzentenos@unjbg.edu.pe>
2024-08-03 23:59:17 -0500

    Se creo el archivo doctores.cpp, se agrego la lectura de la cant. de doctores a registrar en la función doc_nuevos

---
+++ doctores.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "doctores.h"
+#include<string>
+using namespace std;
+INFORMACION doc_nuevos(){
+	INFORMACION info[1000];
+	int n;
+	cout<<"Cantidad de doctores: "; cin>>n;
+}
