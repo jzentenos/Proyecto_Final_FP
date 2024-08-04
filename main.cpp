bb457a430bcaff4158c29204eb9429b2947de13e
acatarip <acatarip@unjbg.edu.pe>
2024-08-04 18:47:35 -0500

    main.cpp : Se agrego la base del menu principal

--- main.cpp
+++ main.cpp
@@ -1,4 +1,38 @@
+//Menu Principal
 #include <iostream>
-int main(int argc, char** argv) {
+using namespace std;
+
+int main (){
+	int n;
+	do {
+		cout<<"\n--------------------"<<endl;
+		cout<<"   MENU PRINCIPAL"<<endl;
+		cout<<"--------------------"<<endl;
+		cout<<"1. Sub-menu PACIENTE"<<endl;
+		cout<<"2. Sub-menu DOCTOR"<<endl;
+		cout<<"3. Agendar cita"<<endl;
+		cout<<"0. Salir"<<endl;
+		cout<<"Ingrese opcion: "; cin>> n;
+		
+		switch (n){
+			case 1:
+				cout<<"\nAhora esta en Sub-menu PACIENTE.";
+				break;
+			case 2:
+				cout<<"\nAhora esta en Sub-menu DOCTORES.";
+				break;
+			case 3:
+				cout<<"\nAhora esta en Agendar cita.";
+				break;
+			case 0:
+				cout<<"\nSaliendo...";
+				break;
+			default:
+				cout<<"\nOpcion invalida. Ingrese su opcion de nuevo: ";
+				break;
+		}
+		
+	} while (n!=0);
+	
 	return 0;
 }
